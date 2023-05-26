a0f4bec349a68c5f95e48d39669d56ce00c5ff42
wldnjs1311 <wldnjs1311@naver.com>
2023-05-26 21:51:17 +0900

    Setting

---
+++ Server/ServerCore/Types.h
@@ -0,0 +1,11 @@
+#pragma once
+
+using BYTE = unsigned char;
+using int8 = __int8;
+using int16 = __int16;
+using int32 = __int32;
+using int64 = __int64;
+using uint8 = unsigned __int8;
+using uint16 = unsigned __int16;
+using unit32 = unsigned __int32;
+using unit64 = unsigned __int64;
